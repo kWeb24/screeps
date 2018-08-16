aad0c8f2e0091cf3b38a5bd923b3f8d1ed7d9b42
Kamil Weber <kamilweber24@gmail.com>
2018-08-17 00:17:25 +0200

    Add: Webpack config

---
+++ screeps.com/webpack.config.js
@@ -0,0 +1,50 @@
+/*jshint esversion: 6 */
+
+const path = require('path');
+const webpack = require('webpack');
+const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin({
+  granularLoaderData: false,
+  outputFormat: 'human'
+});
+
+// screeps-webpack-plugin screeps-profiler
+
+module.exports = smp.wrap({
+    entry: ['./src/default/main.js'],
+    output: {
+      path: path.resolve(__dirname, 'default'),
+      filename: 'main.js',
+      library: "App",
+      libraryTarget: "commonjs2",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: [['env', {
+                      "targets": {
+                        "node": "8.9.3"
+                      }
+                    }]]
+                }
+            }
+        ]
+    },
+    plugins: [
+      new WebpackBuildNotifierPlugin({
+        title: "Screeps",
+        logo: path.resolve("./img/favicon.png"),
+        suppressSuccess: false,
+        suppressCompileStart: false
+      })
+    ],
+    stats: {
+        colors: true
+    },
+    mode: 'production',
+});
