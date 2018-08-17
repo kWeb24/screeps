ac0807d363ca9c207c20c6eca681479b331eec82
Kamil Weber <kamilweber24@gmail.com>
2018-08-17 19:53:57 +0200

    Add: Miner prefab

---
+++ screeps.com/src/default/Creeps/Roles/Miner.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 6 */
+
+console.log('>> Loading Miner Role...');
+
+export default class Miner {
+
+  constructor() {
+
+  }
+
+	/** @param {Creep} creep **/
+	run(creep) {
+
+	}
+
+	needsHelp(fromCreep) {
+		return false;
+	}
+}
