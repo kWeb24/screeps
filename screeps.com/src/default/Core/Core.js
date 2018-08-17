334b2ce0e379e1ab3bcf2e571ddf308c4f7db76b
Kamil Weber <kamilweber24@gmail.com>
2018-08-17 19:04:42 +0200

    Remove: Logs was too fancy

--- screeps.com/src/default/Core/Core.js
+++ screeps.com/src/default/Core/Core.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 6 */
 
-console.log('Initializing Core...');
+console.log('>> Loading Core...');
 
 import Logger from '../Utils/Logger.js';
 import Tower from '../Structures/Tower.js';
@@ -15,13 +15,12 @@ export default class Core {
   constructor() {
     this.PARAM_ADAPTIVE_ROLES = true;
     this.LOOP = 1;
-    
+
     console.log('-- -- CORE_LOGGER_ENABLED: ' + LOGGER.ENABLED);
     console.log('-- -- PARAM_ADAPTIVE_ROLES:' + this.PARAM_ADAPTIVE_ROLES);
 
     this.Tower = new Tower();
     this.RoleManager = new RoleManager();
-    LOGGER.stateChange('Constructing game...');
 
     LOGGER.log('Available Roles', 1);
     this.RoleManager.ROLES.forEach((role) => {
@@ -39,53 +38,35 @@ export default class Core {
       message +=') ';
       LOGGER.note(message, 2);
     });
-
-    LOGGER.finish('Done', 2);
-    LOGGER.stateChange('Game Loop...');
   }
 
   loop() {
-    LOGGER.countLoop('&#8634; LOOP ' + this.LOOP + ' START', 1);
     this.clearDeadCreeps();
     this.spawnCreeps();
     this.Tower.run();
     this.runCreeps();
-    LOGGER.countLoop('&#8856; LOOP ' + this.LOOP + ' ENDS', 1);
     this.LOOP++;
   }
 
   clearDeadCreeps() {
-    LOGGER.log('Clearing dead Creeps...', 1);
     for (var name in Memory.creeps) {
       if (!Game.creeps[name]) {
-        LOGGER.note('Deleting: ' + name, 2);
         delete Memory.creeps[name];
       }
     }
   }
 
   spawnCreeps() {
-    LOGGER.log('Spawning Creeps...', 1);
     this.RoleManager.ROLES.forEach((role) => {
       var creeps = _.filter(Game.creeps, (creep) => creep.memory.role == role.role);
 
       if (creeps.length < role.population) {
         var newName = Game.spawns['CipciaObfita'].createCreep(role.genome, undefined, {role: role.role});
-        LOGGER.note('Spawned: ' + newName + ' as ' + role.role, 2);
       }
     });
   }
 
   runCreeps() {
-    LOGGER.log('Running Creeps...', 1);
-    LOGGER.log('Selecting roles... ', 2);
-
-    if (this.PARAM_ADAPTIVE_ROLES) {
-      LOGGER.success('PARAM_ADAPTIVE_ROLES: ' + this.PARAM_ADAPTIVE_ROLES + ' &#8594; GO', 3);
-    } else {
-      LOGGER.note('PARAM_ADAPTIVE_ROLES: ' + this.PARAM_ADAPTIVE_ROLES + ' &#8603; ABORT', 2);
-    }
-
     for (var names in Game.creeps) {
       var creep = Game.creeps[names];
       this.RoleManager.ROLES.forEach((role) => {
@@ -98,7 +79,5 @@ export default class Core {
         }
       });
     }
-
-    LOGGER.finish('Done', 4);
   }
 }
