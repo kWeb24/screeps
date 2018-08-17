64aefa2683ac5a38d97c75c36a864537a94742bf
Kamil Weber <kamilweber24@gmail.com>
2018-08-17 19:54:05 +0200

    Add: Guard prefab

---
+++ screeps.com/src/default/Creeps/Roles/Military/Guard.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 6 */
+
+console.log('>> Loading Guard Role...');
+
+export default class Guard {
+
+  constructor() {
+
+  }
+
+	/** @param {Creep} creep **/
+	run(creep) {
+
+	}
+
+	needsHelp(fromCreep) {
+		return false;
+	}
+}
