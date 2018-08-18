4588086cb1441512aeb5adaad921fc4169f8f87b
Kamil Weber <kamilweber24@gmail.com>
2018-08-18 22:13:55 +0200

    Add: Base memory methods

--- screeps.com/src/default/Prototypes/Creep/CreepPrototype.js
+++ screeps.com/src/default/Prototypes/Creep/CreepPrototype.js
@@ -23,3 +23,34 @@ Object.assign(Creep.prototype, {
     delete this.memory['key'];
   }
 });
+
+Object.assign(Creep.prototype, {
+  job(job = false) {
+    if (!job) {
+      return this.memory.job;
+    } else {
+      this.memory.job = job;
+    }
+  }
+});
+
+Object.assign(Creep.prototype, {
+  target(target = false) {
+    if (!target) {
+      return this.memory.target;
+    } else {
+      this.memory.target = target;
+    }
+  }
+});
+
+
+Object.assign(Creep.prototype, {
+  status(status = false) {
+    if (!status) {
+      return this.memory.status;
+    } else {
+      this.memory.status = status;
+    }
+  }
+});
