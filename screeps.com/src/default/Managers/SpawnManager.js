7cd9aa58c7d02a073b9560736f1e12fc86c24a2f
Kamil Weber <kamilweber24@gmail.com>
2018-08-18 20:56:30 +0200

    Add: SpawnManager prefab

---
+++ screeps.com/src/default/Managers/SpawnManager.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 6 */
+
+console.log('>> Loading SpawnManager...');
+
+export default class SpawnManager {
+
+  constructor() {
+
+  }
+
+	/** @param {Creep} creep **/
+	run(creep) {
+
+	}
+
+	needsHelp(fromCreep) {
+		return false;
+	}
+}
