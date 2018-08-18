30bde7d101726aecdb2168f688af74051349ae54
Kamil Weber <kamilweber24@gmail.com>
2018-08-18 20:56:12 +0200

    Add: Hauler prefab

---
+++ screeps.com/src/default/Creeps/Roles/Hauler.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 6 */
+
+console.log('>> Loading Hauler Role...');
+
+export default class Hauler {
+
+  constructor() {
+
+  }
+
+	/** @param {Creep} creep **/
+	run(creep) {
+
+	}
+
+	needsHelp(fromCreep) {
+		return false;
+	}
+}
