b1455e1021319a033490325a509726e99a26cb5a
Kamil Weber <kamilweber24@gmail.com>
2018-08-19 19:20:44 +0200

    Change: Singletons and prototypes extensions should be in global scope

--- screeps.com/src/default/Core/Core.js
+++ screeps.com/src/default/Core/Core.js
@@ -3,13 +3,7 @@
 console.log('>> Loading Core...');
 
 import Logger from '../Utils/Logger.js';
-
-import {} from '../Prototypes/Prototypes.js';
-
-import Planner from '../Planners/Planner.js';
-
 import Tower from '../Structures/Tower.js';
-import RoleManager from '../Managers/RoleManager.js';
 
 const LOGGER = new Logger({
   enabled: false
@@ -25,14 +19,10 @@ export default class Core {
     console.log('-- -- CORE_LOGGER_ENABLED: ' + LOGGER.ENABLED);
     console.log('-- -- PARAM_ADAPTIVE_ROLES:' + this.PARAM_ADAPTIVE_ROLES);
 
-    this.Planner = new Planner();
-
     this.Tower = new Tower();
-    this.RoleManager = new RoleManager();
   }
 
   loop() {
-    // this.Planner.ROOMS['W52S54'].drawVisuals();
     this.clearDeadCreeps();
     this.spawnCreeps();
     this.Tower.run();
@@ -49,7 +39,7 @@ export default class Core {
   }
 
   spawnCreeps() {
-    this.RoleManager.ROLES.forEach((role) => {
+    ROLE_MANAGER.ROLES.forEach((role) => {
       var creeps = _.filter(Game.creeps, (creep) => creep.memory.role == role.role);
       if (creeps.length < role.population) {
         var newName = Game.spawns['CipciaObfita'].createCreep(role.genome, undefined, {role: role.role});
@@ -60,10 +50,10 @@ export default class Core {
   runCreeps() {
     for (var names in Game.creeps) {
       var creep = Game.creeps[names];
-      this.RoleManager.ROLES.forEach((role) => {
+      ROLE_MANAGER.ROLES.forEach((role) => {
         if (creep.memory.role == role.role) {
           if (this.PARAM_ADAPTIVE_ROLES) {
-            this.RoleManager.selectRole(creep);
+            ROLE_MANAGER.selectRole(creep);
           } else {
             role.run(creep);
           }
--- screeps.com/src/default/main.js
+++ screeps.com/src/default/main.js
@@ -1,9 +1,15 @@
 /*jshint esversion: 6 */
 
+import {} from './Prototypes/Prototypes.js';
+
 import Core from './Core/Core.js';
 import GlobalCache from './Cache/GlobalCache.js';
+import Planner from './Planners/Planner.js';
+import RoleManager from './Managers/RoleManager.js';
 
 global.CACHE = new GlobalCache();
-const CORE = new Core();
+global.CORE = new Core();
+global.PLANNER = new Planner();
+global.ROLE_MANAGER = new RoleManager();
 
 module.exports.loop = () => CORE.loop();
