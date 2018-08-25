5e4f9aa5b9f720d7fe9150933a788ff897ee32fd
Kamil Weber <kamilweber24@gmail.com>
2018-08-25 15:15:53 +0200

    Add: Spawn manager and utils

--- screeps.com/src/default/main.js
+++ screeps.com/src/default/main.js
@@ -6,15 +6,21 @@ import Core from './Core/Core.js';
 import GlobalCache from './Cache/GlobalCache.js';
 import Planner from './Planners/Planner.js';
 import RoleManager from './Managers/RoleManager.js';
+import SpawnManager from './Managers/SpawnManager.js';
+import Utilities from './Utils/Utilities.js';
 
 global.CACHE = new GlobalCache();
 global.CORE = new Core();
 global.PLANNER = new Planner();
 global.ROLE_MANAGER = new RoleManager();
+global.SPAWN_MANAGER = new SpawnManager();
+global.UTILS = new Utilities();
 
 module.exports.loop = () => CORE.loop();
 
 // TODO: Get shit done
+
+// memory home point
 
 // role tier system
 // build builders only if construction site is present
