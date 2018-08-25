cc9155800c8661c1df27e969d7edd0d75f7712fb
Kamil Weber <kamilweber24@gmail.com>
2018-08-25 17:35:44 +0200

    Add: New source related methods

--- screeps.com/src/default/Prototypes/Creep/Jobs/Harvester.js
+++ screeps.com/src/default/Prototypes/Creep/Jobs/Harvester.js
@@ -9,6 +9,18 @@ Object.assign(Creep.prototype, {
 });
 
 Object.assign(Creep.prototype, {
+  getActiveSources() {
+    return CACHE.ROOMS[this.room.name].getActiveSources();
+  }
+});
+
+Object.assign(Creep.prototype, {
+  getClosestActiveSource() {
+    return this.pos.findClosestByRange(FIND_ACTIVE_SOURCES);
+  }
+});
+
+Object.assign(Creep.prototype, {
   getEnergySinks() {
     return CACHE.ROOMS[this.room.name].getEnergySinks();
   }
