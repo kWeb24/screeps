17334e8ca56dcacadb719216bd8e61ffc5d1aa7b
Kamil Weber <kamilweber24@gmail.com>
2018-08-25 14:19:08 +0200

    Add: Utilities class with GUID generator

---
+++ screeps.com/src/default/Utils/Utilities.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6 */
+
+console.log('>> Loading Utilities...');
+
+export default class Utilities {
+
+  constructor() {
+
+  }
+
+  guidGenerator() {
+    let S4 = () => {
+      return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+  }
+}
