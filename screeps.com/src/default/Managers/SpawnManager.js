c366cf7ac098bcc0e03ca1ba2f66ea7817bd6cc5
Kamil Weber <kamilweber24@gmail.com>
2018-08-28 00:30:00 +0200

    Add: Build custom genomes based on current energy cap

--- screeps.com/src/default/Managers/SpawnManager.js
+++ screeps.com/src/default/Managers/SpawnManager.js
@@ -11,21 +11,6 @@ export default class SpawnManager {
 
   constructor() {
     /**
-    * @member {Object} SpawnManager#BODY_PARTS_PRICES
-    * @desc Set fixed energy price for each body part
-    **/
-    this.BODY_PARTS_PRICES = {
-      move: 50,
-      work: 100,
-      carry: 50,
-      attack: 80,
-      rangedAttack: 150,
-      heal: 250,
-      claim: 600,
-      tough: 10
-    };
-
-    /**
     * @member {Integer} SpawnManager#MAX_BODY_PARTS
     * @desc Set max {@link https://docs.screeps.com/api/#Creep|Screeps Creep}
     * body parts limit
@@ -43,11 +28,48 @@ export default class SpawnManager {
       var creeps = _.filter(Game.creeps, (creep) => creep.memory.role == role.ROLE);
       if (role.shouldSpawn()) {
         const NAME = role.ROLE + '_' + UTILS.guidGenerator();
-        Game.spawns['CipciaObfita'].createCreep(role.GENOME, NAME, {
+        const ROOM = Game.spawns['CipciaObfita'].room;
+        const GENOME = this.createAffordableGenome(role, ROOM);
+
+        if (!GENOME) return false;
+        
+        Game.spawns['CipciaObfita'].createCreep(GENOME, NAME, {
           role: role.ROLE,
-          primarySource: role.getPrimarySource(Game.spawns['CipciaObfita'].room)
+          primarySource: role.getPrimarySource(ROOM)
         });
       }
     });
+  }
+
+  /**
+   * @memberof SpawnManager
+   * @desc Create body parts array that could be spawned with given energy amount
+   * @private
+   **/
+  createAffordableGenome(role, room) {
+    const maxPrice = room.energyAvailable();
+    const possiblePrice = room.energyCapacityAvailable();
+    let currentPrice = 0;
+    let finalGenome = [];
+
+    let search = true;
+    while (currentPrice < maxPrice && search) {
+      role.GENOME.forEach((part) => {
+        let priceOffset = 0;
+
+        if (part == 'carry') {
+          priceOffset = BODYPART_COST[MOVE];
+        }
+
+        if (currentPrice + BODYPART_COST[part] + priceOffset < maxPrice) {
+          finalGenome += part;
+          currentPrice += BODYPART_COST[part];
+        } else {
+          search = false;
+        }
+      });
+    }
+
+    return (finalGenome.length < role.GENOME) ? false : finalGenome;
   }
 }
